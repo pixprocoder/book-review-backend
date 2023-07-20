078296e4ad2f73a1001d301cfb2e0400636c2b3f
skpixels <makkobir79@gmail.com>
2023-07-21 00:36:06 +0300

    server  setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import data from "../data/data.json";
 
 dotenv.config();
 
 const app: Express = express();
 const port = 5000;
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+app.get("/books", (req: Request, res: Response) => {
+  res.send(data);
 });
 
 app.listen(port, () => {
