cc2d8f2594581f3da311761353dc13a9046fe711
samsul kobir <makkobir79@gmail.com>
2024-01-01 15:12:59 +0200

    cokie parse added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express, { Application } from "express";
 import routes from "./app/routes";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 // Parser and Middlewares
-app.use(cors({ origin: "*" }));
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
